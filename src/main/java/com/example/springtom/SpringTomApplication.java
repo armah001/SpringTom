854281ed20a296e016d39ad3f5197d103900767f
Armah <nii.tettey@amalitech.com>
2024-04-23 15:51:01 +0000

    updates

---
+++ src/main/java/com/example/springtom/SpringTomApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTomApplication.class, args);
+    }
+
+}
