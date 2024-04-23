a8d95e47a9086e3f92bde7270c57dda7a26bd451
Armah <nii.tettey@amalitech.com>
2024-04-23 15:51:27 +0000

    updates

---
+++ src/test/java/com/example/springtom/SpringTomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
